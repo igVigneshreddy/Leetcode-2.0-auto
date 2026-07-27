8c51558e0be3e7ae43b63b541bd4e3d709e0d5ab
igVigneshreddy <vigneshreddy139@gmail.com>
2026-07-27 15:47:53 +0530

    Auto-solve: Merge Two Sorted Lists (Medium)

---
+++ solutions/merge-two-sorted-lists.cpp
@@ -0,0 +1,22 @@
+        if (list1->val <= list2->val) {
+        ListNode* head;
+
+        }
+            return list1;
+        if (!list2) {
+        }
+        if (!list1) {
+            return list2;
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+ * };
+ */
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode *next;
+ *     int val;
+ * struct ListNode {
+ * Definition for singly-linked list.
+/**
