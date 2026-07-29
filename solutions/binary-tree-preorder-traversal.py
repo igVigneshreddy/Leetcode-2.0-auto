21a4b6828a3e76e81de56514d5aac90f8b0f1170
igVigneshreddy <vigneshreddy139@gmail.com>
2026-07-29 12:21:38 +0530

    Auto-solve: Binary Tree Preorder Traversal (Medium)

---
+++ solutions/binary-tree-preorder-traversal.py
@@ -0,0 +1,15 @@
+        if not root:
+            return []
+
+        result = []
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        
+        return result
